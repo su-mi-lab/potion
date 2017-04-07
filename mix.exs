62aacb7068cc8f416fc24644bf7cea2f771e7a62
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-04-07 10:40:50 +0900

    Verstion

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Potion.Mixfile do
 
   def project do
     [app: :potion,
-     version: "1.5.1",
+     version: "1.5.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
